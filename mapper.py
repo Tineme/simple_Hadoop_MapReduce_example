9a5e1211566789e2ee8322575d7dcddd7b92e79e
Tineme <84669862+Tineme@users.noreply.github.com>
2023-05-20 00:30:46 -0600

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python
 import sys
-
+import re
 # get all lines from stdin
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
+    
+    #make lower case
+    line = line.lower()
+    
+    #remove punctuations
+    line = re.sub(r'[^\w\s]', '', line)
+    
+    #output tuples (word, 1) in tab-delimited format
+    stopwords = set(['the', 'and', 'a', 'for])
 
     # split the line into words; splits on any whitespace
     words = line.split()
 
     # output tuples (word, 1) in tab-delimited format
     for word in words:
-        print '%s\t%s' % (word, "1")
+        if word not in stopwords:
+         print '%s\t%s' % (word, "1")
