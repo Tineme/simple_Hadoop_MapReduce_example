1bec079698b433054df9a6f9bd2124546b9c5031
Christine Tran <christine6tran@gmail.com>
2023-05-20 06:14:37 +0000

    added 2 more stopwords

--- mapper.py
+++ mapper.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 import sys
-
+import re
 # get all lines from stdin
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
-
+    # make lower case
+    line = line.lower()
     # split the line into words; splits on any whitespace
     words = line.split()
+    # remove punctuation
+    words = re.sub(r'[^\w\s]', '', words)
+
 
     # output tuples (word, 1) in tab-delimited format
+    stopwords = set(['the', 'and']
     for word in words:
+       if word not in stopwords:
         print '%s\t%s' % (word, "1")
